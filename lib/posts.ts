a14afe0623eec662ac43f445d83c61892a152bdf
MyferIsADev <contactme.myfer@protonmail.com>
2025-06-10 20:56:48 -0500

    add new post data

--- lib/posts.ts
+++ lib/posts.ts
@@ -8,6 +8,15 @@ export const posts = [
     },
     name: "switching from obsidian to notion",
   },
+  {
+    post: "/p/",
+    postdate: {
+      month: "june",
+      date: 10,
+      year: 2025,
+    },
+    name: "i just made a new api",
+  },
 ].sort(
   (a, b) =>
     a.postdate.year + a.postdate.date - (b.postdate.year + b.postdate.date)
