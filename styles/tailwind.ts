cb1ee20285260131c2e73e1c04a0e67bd1d0b330
myferr <contactme.myfer@protonmail.com>
2025-08-01 21:50:33 -0500

    feat(styles): add styles/ folder with global and tailwind styles

---
+++ styles/tailwind.ts
@@ -0,0 +1,7 @@
+export const TailwindStyles = {
+  h1: "text-white font-extrabold text-3xl",
+  h2: "text-white font-bold text-2xl",
+  p: "text-lg max-w-xl",
+  link: "text-blue-400 underline decoration-dashed",
+  code: "font-mono bg-[#27272a] px-[5px] py-[2px] mx-1",
+};
